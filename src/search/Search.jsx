a53527b6ceb4ac17d7a1ba1ac77d958c1886e9cb
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-09-07 22:38:26 +0200

    first draft search fn

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -1,21 +1,60 @@
+// Search.jsx
 import { Autocomplete, TextField } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export function SearchBar() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.elements.search.value); // Sets searchInput state to form input value
+  };
+
+  // When the user submits the form, handleSearchSubmit sets the searchInput state.
+  // useEffect detects changes in searchInput and calls fetchSearchResults.
+  // The fetchSearchResults function fetches the data from the API, awaits the response, and updates the state with the results. */
+
+  // API fetches when searchInput changes
+  useEffect(() => {
+    // Define an async function to fetch data
+    const fetchSearchResults = async () => {
+      if (searchInput) {
+        try {
+          const response = await fetch(
+            `https://v2.api.noroff.dev/venues?search=${searchInput}`
+          );
+          const data = await response.json();
+          if (data && Array.isArray(data.data)) {
+            setSearchResults(data.data); // Set search results to the venues array
+          } else {
+            setSearchResults([]); // Reset to an empty array if error
+          }
+        } catch (error) {
+          console.error("Error fetching search results:", error);
+        }
+      }
+    };
+
+    fetchSearchResults();
+  }, [searchInput]); // This useEffect will trigger whenever searchInput changes
+
   return (
-    <Autocomplete
-      freeSolo
-      id="search-bar"
-      // API search here later?
-      options={[]}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search venues by tag(?)"
-          variant="outlined"
-        />
-      )}
-      sx={{ width: "100%", maxWidth: 400 }} // 100% = shrink on smaller screens
-    />
+    <form onSubmit={handleSearchSubmit}>
+      <Autocomplete
+        freeSolo
+        id="search-bar"
+        options={searchResults.map((venue) => venue.name || "")}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            name="search"
+            label="Search venues by name"
+            variant="outlined"
+          />
+        )}
+        sx={{ width: "100%", maxWidth: 400, minWidth: 200 }}
+      />
+    </form>
   );
 }
